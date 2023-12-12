2aec5768d91f03a9df65098b33509c9e0c7eed57
GliczDev <67753196+GliczDev@users.noreply.github.com>
2023-12-12 17:40:37 +0100

    [ci skip] Simplify 1.20.3/4 NMS impl

--- nms/v1_20_R3/src/main/java/me/glicz/holograms/nms/v1_20_R3/NMS_v1_20_R3.java
+++ nms/v1_20_R3/src/main/java/me/glicz/holograms/nms/v1_20_R3/NMS_v1_20_R3.java
@@ -1,10 +1,8 @@
 package me.glicz.holograms.nms.v1_20_R3;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import io.papermc.paper.adventure.PaperAdventure;
 import me.glicz.holograms.line.HologramLine;
 import me.glicz.holograms.nms.v1_20_R2.NMS_v1_20_R2;
-import net.kyori.adventure.text.Component;
 import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -13,7 +11,6 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.ItemStack;
 import org.bukkit.entity.Player;
 
 public class NMS_v1_20_R3 extends NMS_v1_20_R2 {
@@ -44,21 +41,5 @@ public class NMS_v1_20_R3 extends NMS_v1_20_R2 {
         } catch (CommandSyntaxException ex) {
             throw new RuntimeException(ex.getMessage());
         }
-    }
-
-    @Override
-    protected SynchedEntityData.DataValue<?> itemLineToEntityData(Player player, HologramLine<?> line) {
-        return SynchedEntityData.DataValue.create(
-                new EntityDataAccessor<>(23, EntityDataSerializers.ITEM_STACK),
-                ItemStack.fromBukkitCopy((org.bukkit.inventory.ItemStack) line.getContent(player))
-        );
-    }
-
-    @Override
-    protected SynchedEntityData.DataValue<?> textLineToEntityData(Player player, HologramLine<?> line) {
-        return SynchedEntityData.DataValue.create(
-                new EntityDataAccessor<>(23, EntityDataSerializers.COMPONENT),
-                PaperAdventure.asVanilla((Component) line.getContent(player))
-        );
     }
 }
