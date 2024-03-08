e982882f5f13cdb9371b7861e989f7490e5fa047
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-03-08 23:45:20 +0100

    Use my maven repository instead of JitPack

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -15,8 +15,17 @@ java {
 }
 
 publishing {
+    repositories {
+        val repoType = if (version.toString().endsWith("-SNAPSHOT")) "snapshots" else "releases"
+        maven("https://repo.roxymc.net/${repoType}") {
+            name = "roxymc"
+            credentials(PasswordCredentials::class)
+        }
+    }
     publications {
-        create<MavenPublication>("publishMaven") {
+        create<MavenPublication>("maven") {
+            artifactId = "${rootProject.name.lowercase()}-${project.name.lowercase()}"
+
             from(components["java"])
         }
     }
